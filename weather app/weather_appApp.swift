a633924c32ca9bf7f0a827a3417e3414748944dc
Aleksey Kravchuk <aleksey.krauchuk@gmail.com>
2024-08-15 18:06:49 +0200

    Initial Commit

---
+++ weather app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather app
+//
+//  Created by Aleksey Krauchuk on 15/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
